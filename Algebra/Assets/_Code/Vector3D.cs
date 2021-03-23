039052f9c4b22f4ed166a8eb2b8b7dabc8a17661
Fernando747 <fernandojcr5@gmail.com>
2021-03-23 11:21:15 -0600

    Matriz de traslación

--- Algebra/Assets/_Code/Vector3D.cs
+++ Algebra/Assets/_Code/Vector3D.cs
@@ -7,7 +7,10 @@ public class Vector3D : MonoBehaviour
     [Header ("Vectores")]
     [SerializeField] private GameObject gameObjectOne;
     [SerializeField] private GameObject gameObjectTwo;
- 
+    [Header("Traslaciones")]
+    [SerializeField] private float X;
+    [SerializeField] private float Y;
+    [SerializeField] private float Z;
 
 
 
@@ -17,6 +20,16 @@ public class Vector3D : MonoBehaviour
         Debug.Log("Normalizado " + normalize(parseVector(gameObjectOne)));
         Debug.Log("Distancia " + distance(parseVector(gameObjectOne),parseVector(gameObjectTwo)));
         Debug.Log("Cruzado " + cross(parseVector(gameObjectOne), parseVector(gameObjectTwo)));
+        Move();
+    }
+
+    public void Move()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            gameObjectOne.transform.position = transformVector(parseVector(gameObjectOne), X, Y, Z);
+            Debug.Log("Espacio presionado");
+        }
     }
 
     Vector3 parseVector(GameObject objectOne)
@@ -57,6 +70,65 @@ public class Vector3D : MonoBehaviour
         answer.x = (vectorOne.y * vectorTwo.z) - (vectorOne.z * vectorTwo.y);
         answer.y = (vectorOne.z * vectorTwo.x) - (vectorOne.x * vectorTwo.z);
         answer.z = (vectorOne.x * vectorTwo.y) - (vectorOne.y * vectorTwo.x);
+        return answer;
+    }
+
+    public Vector3 transformVector(Vector3 Vector, float X, float Y, float Z)
+    {
+        Vector3 answer = new Vector3();
+        float[,] delta = generateMatrix();
+        float[] vectorMatrix = new float[4];
+        vectorMatrix[0] = Vector.x;
+        vectorMatrix[1] = Vector.y;
+        vectorMatrix[2] = Vector.z;
+        vectorMatrix[3] = 1;
+        delta[0 ,3] = X;
+        delta[1, 3] = Y;
+        delta[2, 3] = Z;
+        float[] multiplied = multiplyMatrix3D(delta,vectorMatrix);
+        answer.x = multiplied[0];
+        answer.y = multiplied[1];
+        answer.z = multiplied[2];
+        return answer;
+    }
+
+    float[] multiplyMatrix3D(float[,] delta, float[] Vector)
+    {
+        float[] answer = new float[4];
+        float temporal = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                temporal = temporal + (delta[i, x] * Vector[i]);
+                if (x == 3)
+                {
+                    answer[i] = temporal;
+                }
+            }
+           temporal = 0;
+        }
+        return answer;
+    } 
+
+     float[,] generateMatrix()
+    {
+        float[,] answer = new float[4, 4];
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                if (i == x)
+                {
+                    answer[i, x] = 1;
+                }
+                else
+                {
+                    answer[i, x] = 0;
+                }
+            }
+        }
         return answer;
     }
 }
